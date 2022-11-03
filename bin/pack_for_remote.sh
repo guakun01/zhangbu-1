2012f87eea3272222063af421d3bcff3edf0ef2c
guakun01 <guakun01@gmail.com>
2022-11-03 22:41:40 +0800

    执行数据库变更出错

---
+++ bin/pack_for_remote.sh
@@ -0,0 +1,37 @@
+user=zhangbu-1
+ip=101.43.49.196
+
+time=$(date +'%Y%m%d-%H%M%S')
+dist=tmp/zhangbu-$time.tar.gz
+current_dir=$(dirname $0)
+deploy_dir=/home/$user/deploys/$time
+gemfile=$current_dir/../Gemfile
+gemfile_lock=$current_dir/../Gemfile.lock
+
+function title {
+  echo
+  echo "################################################################################"
+  echo "## $1"
+  echo "################################################################################"
+  echo
+}
+
+yes | rm tmp/zhangbu-*.tar.gz;
+
+title '打包源代码为压缩文件'
+bundle cache
+tar --exclude="tmp/cache/*" -czv -f $dist *
+title '创建远程目录'
+ssh $user@$ip "mkdir -p $deploy_dir"
+title '上传压缩文件'
+scp $dist $user@$ip:$deploy_dir/
+scp $gemfile $user@$ip:$deploy_dir/
+scp $gemfile_lock $user@$ip:$deploy_dir/
+title '上传 Dockerfile'
+scp $current_dir/../config/host.Dockerfile $user@$ip:$deploy_dir/Dockerfile
+title '上传 setup 脚本'
+scp $current_dir/setup_remote.sh $user@$ip:$deploy_dir/
+title '上传版本号'
+ssh $user@$ip "echo $time > $deploy_dir/version"
+title '执行远程脚本'
+ssh $user@$ip "export version=$time; /bin/bash $deploy_dir/setup_remote.sh"
